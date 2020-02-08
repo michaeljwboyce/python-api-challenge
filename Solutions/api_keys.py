c14daa2b98b06e9730aeb021e14dc51a296c212f
michaeljwboyce <michaeljwboyce@gmail.com>
2020-02-08 17:19:31 -0500

    2nd commit

--- Solutions/api_keys.py
+++ Solutions/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "89697732c8e7b9c7e42a048fa5bb3969"
 
 # Google API Key
-g_key = "YOUR KEY HERE"
+g_key = "AIzaSyC-kpiNtGptOR5G8DVlJFI_KIw0a3i7UhE"
